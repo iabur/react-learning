038ba4d760dd64c618121d1cb9b8c86f89223f13
iabur <iaburrahman1@gmail.com>
2025-05-21 23:25:41 +0600

    Fix reference handling in App component to correctly apply dynamic styling to the heading on button click

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ import React, { useRef } from 'react';
 const App = () => {
   let h1Ref = useRef(null);
   const handleClick = () => {
-    h1Ref.current.style.color = "red";
+    h1Ref.style.color = "red";
   }
   return (
     <div>
-      <h1 ref={h1Ref}>Hello World</h1>
+      <h1 ref={(p) => h1Ref = p}>Hello World</h1>
       <button onClick={handleClick}>Click Me</button>
     </div>
   );
