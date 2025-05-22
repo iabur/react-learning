57747fb969472ec6fa0b5fc266340724210892a3
iabur <iaburrahman@brainstation-23.com>
2025-05-22 18:42:39 +0600

    Enhance App component by adding an image and button that dynamically adjusts image size on click using useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
-import HubspotForm from './component/HubspotForm';
 
 const App = () => {
+  let imgRef = useRef(null);
+  const handleClick = () => {
+    imgRef.current.setAttribute("height", "1000px");
+    imgRef.current.setAttribute("width", "1000px");
+  }
   return (
     <div>
-       <HubspotForm />
+      <img ref={imgRef} src="https://placehold.co/600x400" alt="placeholder" />
+       <button onClick={handleClick}>Click Me</button>
     </div>
   );
 };
