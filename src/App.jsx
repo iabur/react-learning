d3d6ac2ff3bc805cc404895796e6c7ce1a041d43
iabur <iaburrahman@brainstation-23.com>
2025-05-23 16:46:44 +0600

    Refactor App component to manage a list of items with useState, adding functionality to add and delete items dynamically, and updating the UI to display the list in a table format.

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,45 @@ import React, { useState } from 'react';
 
 
 const App = () => {
-  const [person, setPerson] = useState({
-    name: 'Iabur',
-    age: 30
-  });
- 
+  const [list, setList] = useState([]);
+  const [item, setItem] = useState('');
+
   const handleClick = () => {
-    setPerson(previousPerson => ({
-        ...previousPerson,
-        age: previousPerson.age + 1
-    }))
+    list.push(item);
+    setList([...list]);
   }
+
+
+  const handleDelete = (index) => {
+    list.splice(index, 1);
+    setList([...list]);
+  }
+
 
   return (
     <div>
-       <h1>Name: {person.name}</h1>
-       <h1>Age: {person.age}</h1>
-       <button onClick={handleClick} className='btn btn-primary'>Click Me</button>
+       <input type='text' value={item} onChange={e => setItem(e.target.value)} />
+       <button onClick={handleClick} className='btn btn-primary'>Add</button>
+       
+       <table>
+        <tbody>
+          {list.length > 0 ? (
+            list.map((item, index) => (
+              <tr key={index }>
+                <td>{item}</td>
+                <td>
+                  <button className='btn btn-danger' onClick={() => handleDelete(index)}>Delete</button>
+                </td>
+              </tr>
+            
+            ))
+          ) : (
+            <tr>
+              <td colSpan={2}>No data</td>
+            </tr>
+          )}
+        </tbody>
+       </table>
     </div>
   );
 };
