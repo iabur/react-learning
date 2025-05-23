ea03d6f2ccf953c59ac040ee9a9776ba93e61bb6
iabur <iaburrahman@brainstation-23.com>
2025-05-23 18:10:54 +0600

    Refactor App component to remove form inputs and replace them with a simple heading, while adding a useEffect hook for logging on component mount.

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,18 @@
 import { Dropdown } from 'bootstrap';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 
 const App = () => {
 
-  const [person, setPerson] = useState({
-    fName: '',
-    lName: '',
-    city: '',
-    gender: 'male',
-  })
-
-  const setValue = (field, value) => {
-    setPerson(prev => ({...prev, [field]: value}))
-  } 
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    alert(JSON.stringify(person))
-  }
+  useEffect(() => {
+    console.log('Mounting')
+  }, [])
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-      <input type='text' value={person.fName} onChange={e => setValue('fName', e.target.value)} />
-      <input type='text' value={person.lName} onChange={e => setValue('lName', e.target.value)} />
-      <select value={person.city} onChange={e => setValue('city', e.target.value)}>
-        <option value="new-york">New York</option>
-        <option value="los-angeles">Los Angeles</option>
-        <option value="chicago">Chicago</option>
-        <option value="houston">Houston</option>
-        <option value="miami">Miami</option>
-        <option value="san-francisco">San Francisco</option>
-      </select>
-      <input type='radio' name='gender' value="male" checked={person.gender === 'male'} onChange={e => setValue('gender', e.target.value)} /> Male
-      <input type='radio' name='gender' value="female" checked={person.gender === 'female'} onChange={e => setValue('gender', e.target.value)} /> Female 
-      <input type='radio' name='gender' value="other" checked={person.gender === 'other'} onChange={e => setValue('gender', e.target.value)} /> Other
-      <button>Submit</button>
-      </form>
+      <h1>Hello World</h1>
     </div>
+
   );
 };
 
